eb2a0d73a78f1a14369f41ddda108e17a9fb737a
yuanxiwang <837230933@qq.com>
2019-07-21 22:05:41 +0800

    内容content
    添加首页文章内容content

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,13 +2,15 @@
 //获取应用实例
 const app = getApp()
 var call = require("../../utils/request.js")
+var page = 0;
 
 Page({
   /**
    * 页面初始化数据
    */
   data: {
-    imgPaths: [{ "desc": "Android高级进阶直播课免费学习", "id": 23, "imagePath": "https://wanandroid.com/blogimgs/0b712568-6203-4a03-b475-ff55e68d89e8.jpeg", "isVisible": 1, "order": 0, "title": "Android高级进阶直播课免费学习", "type": 0, "url": "https://url.163.com/4bj" }, { "desc": "一起来做个App吧", "id": 10, "imagePath": "https://www.wanandroid.com/blogimgs/50c115c2-cf6c-4802-aa7b-a4334de444cd.png", "isVisible": 1, "order": 0, "title": "一起来做个App吧", "type": 1, "url": "http://www.wanandroid.com/blog/show/2" }, { "desc": "", "id": 6, "imagePath": "https://www.wanandroid.com/blogimgs/62c1bd68-b5f3-4a3c-a649-7ca8c7dfabe6.png", "isVisible": 1, "order": 1, "title": "我们新增了一个常用导航Tab~", "type": 1, "url": "http://www.wanandroid.com/navi" }
+    airtcleList: [{ "apkLink": "", "author": "stevewang", "chapterId": 93, "chapterName": "基础知识", "collect": false, "courseId": 13, "desc": "", "envelopePic": "", "fresh": false, "id": 8690, "link": "https://www.jianshu.com/p/cb5181418c7a", "niceDate": "2019-07-12", "origin": "", "prefix": "", "projectLink": "", "publishTime": 1562861151000, "superChapterId": 94, "superChapterName": "自定义控件", "tags": [], "title": "Android TouchDelegate详解及优化", "type": 0, "userId": -1, "visible": 1, "zan": 0 }],
+    imgPaths: [{ "desc": "Android高级进阶直播课免费学习", "id": 23, "imagePath": "https://wanandroid.com/blogimgs/0b712568-6203-4a03-b475-ff55e68d89e8.jpeg", "isVisible": 1, "order": 0, "title": "Android高级进阶直播课免费学习", "type": 0, "url": "https://url.163.com/4bj" }
     ],
     //指示器
     indecatorDots:true,
@@ -25,61 +27,7 @@ Page({
     //前边距，可用于露出前一项的一小部分，接受px和rpx
     perviousMargin:0,
     //后边距，可用于露出后一项的一小部分，接受px和rpx
-    nextMargin:0,
-    contentList:[{
-      "apkLink": "",
-      "author": "mseddl ",
-      "chapterId": 245,
-      "chapterName": "集合相关",
-      "collect": false,
-      "courseId": 13,
-      "desc": "",
-      "envelopePic": "",
-      "fresh": false,
-      "id": 8688,
-      "link": "https://www.jianshu.com/p/e1c020d37c6a",
-      "niceDate": "2019-07-12",
-      "origin": "",
-      "prefix": "",
-      "projectLink": "",
-      "publishTime": 1562861004000,
-      "superChapterId": 245,
-      "superChapterName": "Java深入",
-      "tags": [],
-      "title": "详解并发下的HashMap以及JDK8的优化",
-      "type": 0,
-      "userId": -1,
-      "visible": 1,
-      "zan": 0
-    }, {
-        "apkLink": "",
-        "author": "xiaoyang",
-        "chapterId": 440,
-        "chapterName": "官方",
-        "collect": false,
-        "courseId": 13,
-        "desc": "<p>和<a href=\"https://www.wanandroid.com/wenda/show/8672\" target=\"_blank\" style=\"font-size: 16px;\">上一个问题</a>&nbsp;当成兄弟问题咯。</p><br><p>如果可以，为什么？</p>",
-        "envelopePic": "",
-        "fresh": false,
-        "id": 8680,
-        "link": "https://www.wanandroid.com/wenda/show/8680",
-        "niceDate": "2019-07-11",
-        "origin": "",
-        "prefix": "",
-        "projectLink": "",
-        "publishTime": 1562858911000,
-        "superChapterId": 440,
-        "superChapterName": "问答",
-        "tags": [{
-          "name": "问答",
-          "url": "/article/list/0?cid=440"
-        }],
-        "title": "每日一问 在Activity 的 onResume 方法中 view.postRunnable 能获取到 View 宽高吗？",
-        "type": 0,
-        "userId": 2,
-        "visible": 1,
-        "zan": 8
-      }]
+    nextMargin:0
   },
   onLoad:function(){
     var that = this;
@@ -103,6 +51,41 @@ Page({
     })
   },
   onPullDownRefresh:function(event){
-    call.request
+    page = 0;
+    call.request("article/list/" + page + "/json",'GET', this.onArticleSuccess, this.onArticleFail);
+  }, 
+  onArticleSuccess:function(data){
+    var that = this;
+    console.log(data);
+    that.setData({
+      airtcleList:data.data.datas
+    })
   },
+  onArticleFail:function(){
+    console.log("刷新失败");
+  },
+  onReachBottom:function(){
+    var that = this;
+    // 显示加载图标
+    wx.showLoading({
+      title: '玩命加载中',
+    })
+    // 页数+1
+    page = page + 1;
+    call.request("article/list/" + page + "/json",'GET', this.onLoadMoreSuccess, this.loadMoreFail);
+  },
+  onLoadMoreSuccess:function(data){
+    var that = this;
+    // 回调函数
+    var airtcleList = that.data.airtcleList;
+    const oldData = that.data.airtcleList;
+    that.setData({
+      airtcleList: oldData.concat(data.data.datas)
+    });
+    // 隐藏加载框
+    wx.hideLoading();
+  },
+  loadMoreFail:function(){
+    console.log("加载失败")
+  }
 })
